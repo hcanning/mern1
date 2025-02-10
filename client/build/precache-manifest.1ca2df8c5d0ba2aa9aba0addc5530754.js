ad8d1d2cfb25f12717af4ee24e233a97adb5575b
hcanning <46687863+hcanning@users.noreply.github.com>
2025-02-10 12:10:36 -0500

    initial

---
+++ client/build/precache-manifest.1ca2df8c5d0ba2aa9aba0addc5530754.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5fc047012ebbe65664f23357f195750",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef67cf4f9d5cd78c946e",
+    "url": "/static/css/main.11b62bc3.chunk.css"
+  },
+  {
+    "revision": "36c68d140a8553eebb51",
+    "url": "/static/js/2.f738b9b9.chunk.js"
+  },
+  {
+    "revision": "ef67cf4f9d5cd78c946e",
+    "url": "/static/js/main.f0edee4d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
